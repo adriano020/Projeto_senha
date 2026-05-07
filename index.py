0d29abe305f461c9a2a03c0c338a01789b83a787
adriano020 <adriano020@gmail.com>
2026-05-06 21:45:59 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+from tkinter import*
+from tkinter import messagebox
+
+cliente = Tk()
+cliente.title("Solicitar Senha")
+cliente.geometry("300x250")
+cliente.configure(bg="lightgreen")
+
+cliente.mainloop()
